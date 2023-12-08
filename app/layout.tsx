a48a4916632eb3c3d2140897b7a7cf5d1c621135
Abdulqadir8604 <web.aq8604@gmail.com>
2023-12-08 16:25:08 +0530

    fixed column error in orders

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type {Metadata} from 'next'
-import {Inter} from 'next/font/google'
+import {Montserrat} from 'next/font/google'
 import {ClerkProvider} from "@clerk/nextjs";
 
 import {ModalProvider} from "@/providers/modal-provider";
@@ -8,7 +8,7 @@ import './globals.css'
 import {ToasterProvider} from "@/providers/toast-provider";
 import {ThemeProvider} from '@/providers/theme-provider';
 
-const inter = Inter({subsets: ['latin']})
+const font = Montserrat({subsets: ['latin']})
 
 export const metadata: Metadata = {
     title: 'Admin Dashboard',
@@ -23,7 +23,7 @@ export default function RootLayout({
     return (
         <ClerkProvider>
             <html lang="en">
-            <body className={inter.className}>
+            <body className={font.className}>
             <ThemeProvider attribute={"class"} defaultTheme={"system"} enableSystem>
                 <ToasterProvider />
                 <ModalProvider />
